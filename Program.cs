531de70e512b2cb00def8795bd2aad33f72bcaba
Emil Malmsten <emil_malmsten@hotmail.com>
2024-01-24 10:57:51 +0700

    first

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿public class TodoItem
+{
+    public string? Description { get; set; }
+
+    public TodoItem(string description)
+    {
+        Description = description;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string? todo = null;
+
+        while (string.IsNullOrEmpty(todo))
+        {
+            Console.WriteLine("Input todo item:");
+            todo = Console.ReadLine();
+            if (string.IsNullOrEmpty(todo))
+            {
+                Console.WriteLine("Todo item can not be empty.");
+            }
+        }
+
+        var todoItem = new TodoItem(todo);
+        Console.WriteLine(todoItem.Description);
+    }
+}
+
+
+
